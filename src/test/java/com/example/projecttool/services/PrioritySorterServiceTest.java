133524e51b7161f7ea5ee49b26c641c5cef20c43
Matthias <mdjer@hotmail.com>
2021-06-01 21:40:03 +0200

    fixed headers in tables

--- src/test/java/com/example/projecttool/services/PrioritySorterServiceTest.java
+++ src/test/java/com/example/projecttool/services/PrioritySorterServiceTest.java
@@ -18,11 +18,11 @@ class PrioritySorterServiceTest {
 
         // Arrange
         ArrayList<Task> taskList = new ArrayList<>();
-        Task task = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06", "low", (byte) 1,true);
-        Task task1 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06","medium", (byte) 1, false);
-        Task task2 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06","high", (byte) 1, true);
-        Task task3 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06","high", (byte) 1, true);
-        Task task4 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06", "high", (byte) 1, false);
+        Task task = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06", "l", (byte) 1,true);
+        Task task1 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06","m", (byte) 1, false);
+        Task task2 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06","h", (byte) 1, true);
+        Task task3 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06","h", (byte) 1, true);
+        Task task4 = new Task(1, "Test", "Test1", "2014-05-05", "2014-06-06", "h", (byte) 1, false);
         taskList.add(task);
         taskList.add(task1);
         taskList.add(task2);
@@ -37,16 +37,16 @@ class PrioritySorterServiceTest {
             if(p1 == null) return 1;
             if(p2 == null) return -1;
             if(p1.equals(p2)) return 0;
-            if(p1.equals("high") && (p2.equals("medium") || p2.equals("low")))
+            if(p1.equals("h") && (p2.equals("m") || p2.equals("l")))
                 return -1;
-            if(p1.equals("medium") && p2.equals("low"))
+            if(p1.equals("m") && p2.equals("l"))
                 return -1;
             return 1;
         });
         // Assert
-        assertEquals(taskList.get(0).getPriority(), "high");
-        assertEquals(taskList.get(3).getPriority(), "medium");
-        assertEquals(taskList.get(4).getPriority(), "low");
+        assertEquals(taskList.get(0).getPriority(), "h");
+        assertEquals(taskList.get(3).getPriority(), "m");
+        assertEquals(taskList.get(4).getPriority(), "l");
 
     }
 
