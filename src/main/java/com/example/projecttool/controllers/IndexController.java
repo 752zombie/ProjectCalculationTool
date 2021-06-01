62a8b0c708274559f4cff6671bfaacec9fe6d4ad
Matthias <mdjer@hotmail.com>
2021-06-01 21:24:23 +0200

    fixed headers in tables

--- src/main/java/com/example/projecttool/controllers/IndexController.java
+++ src/main/java/com/example/projecttool/controllers/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class GeneralController {
+public class IndexController {
     @GetMapping("home")
     public String homePage() {
         return "index";
